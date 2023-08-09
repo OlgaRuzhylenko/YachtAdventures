f6f60ba5aa051e2cafe0dbd272583677d6dff4e6
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-08-09 16:58:39 +0300

    Initialize img/change

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,35 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+  //Додали доп клас, щоб закрити після нажаття на посилання
+  const menuLinks = document.querySelectorAll('.mobile-modal-link');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  //додали, щоб закривалось при натисканні на посилання
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', toggleMenu);
+  })
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import "./js/mobile-menu";
+import "./js/modal";
+import "./js/slider";
+import "./js/back-to-top";
